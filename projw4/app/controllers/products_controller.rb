9fa512d8f357461185dca402527b7da677a371e2
Laura <laura.s.chavez@outlook.com>
2019-02-12 15:15:26 -0500

    move selection to product index

--- projw4/app/controllers/products_controller.rb
+++ projw4/app/controllers/products_controller.rb
@@ -2,9 +2,12 @@
 class ProductsController < ApplicationController
     def index
         
-        # @productsa = fill_product
-        @productsa = Convert.all.where("quantity > 0")
-         @products = @productsa.sort_by { |h| h.category }
+        @categories = Convert.find_by_sql("SELECT category FROM converts GROUP BY category").map &:category
+        @products = params[:category].blank? ? Convert.all.where("quantity > 0") : Listing.find_all_by_category(params[:category])
+      
+        # @productsa = Convert.all.where("quantity > 0")
+        # @selection=params_convert.category
+        #  @products = @productsa.sort_by { |h| h.category }
     end
 
 
@@ -17,7 +20,7 @@ class ProductsController < ApplicationController
      private
 
      def params_convert
-            params.require(:convert).permit(:category)
+            params.permit(:category)
      end
     
 
